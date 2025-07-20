079ed62e9a9f905449dae31821af18fddfce7db7
Jörn Stöhler <joern@stoehler.eu>
2025-07-20 15:14:19 +0000

    feat: wire up tree components with DSL registry

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Import DSL registry and tree components
+import { registry } from './dsl'
+import { TreeView } from './components/tree/TreeView'
+import { GameControls } from './components/GameControls'
+
+// Register components with DSL
+registry.register('TreeView', TreeView)
+registry.register('GameControls', GameControls)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
